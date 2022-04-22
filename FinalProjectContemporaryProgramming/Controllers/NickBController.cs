f2560f0ee5c77c19ed5973eeb0c639c95a6c6f2a
Nick <alphapixelcodes@gmail.com>
2022-04-21 21:27:04 -0400

    Nick made some changes to his controller

--- FinalProjectContemporaryProgramming/Controllers/NickBController.cs
+++ FinalProjectContemporaryProgramming/Controllers/NickBController.cs
@@ -19,22 +19,93 @@ namespace FinalProjectContemporaryProgramming.Controllers
             public string FirstName { get; set; }
             public string LastName { get; set; }
 
-            public string FavoriteTA => "Monish Chamlagai";
-            public string FavoriteTeacher => "Awais Shoaib";
-            public string FavoriteClass => "Contemporary Programming";
+            public string FavoriteTA { get; set; } = "Monish Chamlagai";
+            public string FavoriteTeacher { get; set; } = "Awais Shoaib";
+            public string FavoriteClass { get; set; }= "Contemporary Programming";
+            public bool IsALiar { get; set; }
         }
+        public Nick TheBestNick = new Nick();
+        /// <summary>
+        /// gets every element in the table
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
+        [Route("All")]
         public IEnumerable<Nick> Get()
         {
-            return new List<Nick>() { new Nick() { ID = -1, FirstName = "Nick", LastName = "Bell" } };
+            List<Nick> ret = new List<Nick>();
+            for (int i = 0; i < 5; i++)
+            {
+                ret.Add(new Nick() { ID = i, FirstName = "Nick", LastName = "Bell" });
+            }
+            return ret;
         }
-        [HttpGet()]
-        [Route("Default/NickB{ID=int}")]
-        public Nick Get(int id=-2)
+        [HttpGet]
+        [Route("ByID")]
+        public Nick Get([FromQuery] int id)
         {
             Debug.WriteLine("Im here nick");
             return new Nick() { ID = id, FirstName = "Nick", LastName = "Bell" };
         }
-       
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status406NotAcceptable)]
+        public ActionResult Post([FromQuery]string FirstName, [FromQuery] string LastName, [FromQuery] string FavoriteTA= "Monish Chamlagai", [FromQuery] string FavoriteTeacher= "Awais Shoaib", [FromQuery] string FavoriteClass= "Contemporary Programming", [FromQuery] bool IsALiar=false)
+        {
+            if (!IsALiar)
+            {
+                if (FavoriteTA != TheBestNick.FavoriteTA)
+                {
+                    return StatusCode(406, new CustomResponse() { Title = "Invalid Favorite TA", Message = $"Their Favorite TA clearly isn't '{FavoriteTA}'. If you wish to fill this table with lies then please mark this person as a liar. "});
+                }else if (FavoriteTeacher != TheBestNick.FavoriteTeacher)
+                {
+                    return StatusCode(406, new CustomResponse() { Title = "Invalid Favorite Teacher", Message = $"Their Favorite Teacher clearly isn't '{FavoriteTeacher}'. If you wish to fill this table with lies then please mark this person as a liar. " });
+                }
+                else if(FavoriteClass != TheBestNick.FavoriteClass)
+                {
+                    return StatusCode(406, new CustomResponse() { Title = "Invalid Favorite Class", Message = $"Their Favorite Class clearly isn't '{FavoriteClass}'. if you wish to fill this table with lies then please mark this person as a liar. " });
+                }
+            }
+            Debug.WriteLine("Change NickBController.Post to actually post to database");
+            //Create new database Row 
+            return StatusCode(202,new Nick());
+        }
+        [HttpPatch]
+        public ActionResult Update([FromQuery] int id, [FromQuery] string FirstName=null, [FromQuery] string LastName=null, [FromQuery] string FavoriteTA = null, [FromQuery] string FavoriteTeacher = null, [FromQuery] string FavoriteClass = null, [FromQuery] bool IsALiar = false)
+        {
+            if (GetNickById(id) == null)
+                return StatusCode(404, new { id });
+            Debug.WriteLine("Update by id idk man");
+            return StatusCode(202, new {id,FirstName, LastName,FavoriteTA});
+        }
+        private int GetNextAvailableID()
+        {
+            Debug.WriteLine("Change NickBController.GetNextAvailableID to actually get the next available id");
+            return 0;//todo check database for next available id
+        }
+
+
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Nick> Delete([FromQuery]int id)
+        {
+            if(IdExists(id))
+            {
+                //delete id and return it
+                return Ok(new Nick() { ID = id });
+            }
+            return StatusCode(404,new { id });
+        }
+        private bool IdExists(int id)
+        {
+            return true;
+        }
+        private Nick GetNickById(int id)
+        {
+            //if id exists return nick object, if it doesnt then return null
+            return new Nick() { ID = id };
+        }
     }
 }
---
+++ FinalProjectContemporaryProgramming/CustomResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace FinalProjectContemporaryProgramming
+{
+    //this is for custom responses incase error codes werent good enough
+    public struct CustomResponse
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+    }
+}
