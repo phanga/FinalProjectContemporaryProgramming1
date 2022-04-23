ff9fd385f4cfc9ed3211f270f8beb8e64435effd
Smoofington <caudill.matthew0127@gmail.com>
2022-04-23 05:17:51 -0500

    Added EntityFrameworkCore.SqlServer & .Tools to project. Added connectionStrings to appsettings.JSON

---
+++ FinalProjectContemporaryProgramming/Models/DataLayer/Gabe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace FinalProjectContemporaryProgramming.Models.DataLayer
+{
+    public partial class Gabe
+    {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string FullName { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? Birthdate { get; set; }
+        [StringLength(50)]
+        public string CollegeProgram { get; set; }
+        [StringLength(50)]
+        public string YearInProgram { get; set; }
+    }
+}
