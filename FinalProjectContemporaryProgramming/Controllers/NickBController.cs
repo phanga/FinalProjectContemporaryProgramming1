e35cb463ca59f512ed4e545a9d623e7efb0c66e9
Nick <alphapixelcodes@gmail.com>
2022-04-26 19:31:26 -0400

    Added File Prompt

--- FinalProjectContemporaryProgramming/Controllers/NickBController.cs
+++ FinalProjectContemporaryProgramming/Controllers/NickBController.cs
@@ -16,11 +16,27 @@ namespace FinalProjectContemporaryProgramming.Controllers
     public class NickBController : ControllerBase
     {
 
-        private object NotFoundMessage=new
+        private object NotFoundMessage = new
         {
             Title = "Not Found",
             Message = "There is no row with that ID"
         };
+        private ObjectResult CheckForLies(string FavoriteTA, string FavoriteTeacher, String FavoriteClass)
+        {
+            if (FavoriteTA != null && FavoriteTA != "Monish Chamlagai")
+            {
+                return StatusCode(406, new { Title = "Invalid Favorite TA", Message = $"Their Favorite TA clearly isn't '{FavoriteTA}'. If you wish to fill this table with lies then please mark this person as a liar. " });
+            }
+            else if (FavoriteTeacher != null && FavoriteTeacher != "Awais Shoaib")
+            {
+                return StatusCode(406, new { Title = "Invalid Favorite Teacher", Message = $"Their Favorite Teacher clearly isn't '{FavoriteTeacher}'. If you wish to fill this table with lies then please mark this person as a liar. " });
+            }
+            else if (FavoriteClass != null && FavoriteClass != "Contemporary Programming")
+            {
+                return StatusCode(406, new { Title = "Invalid Favorite Class", Message = $"Their Favorite Class clearly isn't '{FavoriteClass}'. if you wish to fill this table with lies then please mark this person as a liar. " });
+            }
+            return Ok(null);
+        }
 
         [HttpGet]
         [Route("All")]
@@ -29,56 +45,61 @@ namespace FinalProjectContemporaryProgramming.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status302Found)]
         [Route("ByID")]
-        public ActionResult Get([FromQuery] int id)=> IdExists(id)? Ok(GetNickById(id)): StatusCode(404, NotFoundMessage);
-     
-        private ObjectResult CheckForLies(string FavoriteTA, string FavoriteTeacher, String FavoriteClass)
-        {
-            if (FavoriteTA != "Monish Chamlagai")
-            {
-                return StatusCode(406, new { Title = "Invalid Favorite TA", Message = $"Their Favorite TA clearly isn't '{FavoriteTA}'. If you wish to fill this table with lies then please mark this person as a liar. " });
-            }
-            else if (FavoriteTeacher != "Awais Shoaib")
-            {
-                return StatusCode(406, new { Title = "Invalid Favorite Teacher", Message = $"Their Favorite Teacher clearly isn't '{FavoriteTeacher}'. If you wish to fill this table with lies then please mark this person as a liar. " });
-            }
-            else if (FavoriteClass != "Contemporary Programming")
-            {
-                return StatusCode(406, new { Title = "Invalid Favorite Class", Message = $"Their Favorite Class clearly isn't '{FavoriteClass}'. if you wish to fill this table with lies then please mark this person as a liar. " });
-            }
-            return Ok(null);
-        }
+        public ActionResult Get([FromQuery] int id) => IdExists(id) ? Ok(GetNickById(id)) : StatusCode(404, NotFoundMessage);
+
 
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status202Accepted)]
         [ProducesResponseType(StatusCodes.Status406NotAcceptable)]
-        public ActionResult Post([FromQuery]string FirstName, [FromQuery] string LastName, [FromQuery] string FavoriteTA= "Monish Chamlagai", [FromQuery] string FavoriteTeacher= "Awais Shoaib", [FromQuery] string FavoriteClass= "Contemporary Programming", [FromQuery] bool IsALiar=false)
+        public ActionResult Post([FromQuery] string FirstName, [FromQuery] string LastName, [FromQuery] string FavoriteTA = "Monish Chamlagai", [FromQuery] string FavoriteTeacher = "Awais Shoaib", [FromQuery] string FavoriteClass = "Contemporary Programming", [FromQuery] bool IsALiar = false)
         {
             if (!IsALiar)
             {
-                var islying =CheckForLies(FavoriteTA, FavoriteTeacher, FavoriteClass);
-                if (islying.StatusCode == 406)
-                    return islying;
-            }
-            var added = new NicksTable() { Id = GetNextAvailableID(),FirstName=FirstName,LastName=LastName,FavoriteTa=FavoriteTA,FavoriteTeacher=FavoriteTeacher,FavoriteClass=FavoriteClass,IAmALiar=IsALiar };
-            DBContext.Context.Add(added);
-            DBContext.Context.SaveChanges();
-            return StatusCode(202,added);
-        }
-        [HttpPatch]
-        [ProducesResponseType(StatusCodes.Status202Accepted)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult Update([FromQuery] int id, [FromQuery] string FirstName=null, [FromQuery] string LastName=null, [FromQuery] string FavoriteTA = null, [FromQuery] string FavoriteTeacher = null, [FromQuery] string FavoriteClass = null, [FromQuery] bool IsALiar = false)
-        {
-            if (!IdExists(id))
-                return StatusCode(404,NotFoundMessage);
-            if (!IsALiar) { 
                 var islying = CheckForLies(FavoriteTA, FavoriteTeacher, FavoriteClass);
                 if (islying.StatusCode == 406)
                     return islying;
             }
-            
-            Debug.WriteLine("Update by id idk man");
-            return StatusCode(202, new {id,FirstName, LastName,FavoriteTA});
+            var added = new NicksTable() { Id = GetNextAvailableID(), FirstName = FirstName, LastName = LastName, FavoriteTa = FavoriteTA, FavoriteTeacher = FavoriteTeacher, FavoriteClass = FavoriteClass, IAmALiar = IsALiar };
+            DBContext.Context.Add(added);
+            DBContext.Context.SaveChanges();
+            return StatusCode(202, added);
+        }
+        [HttpPatch]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult Update([FromQuery] int id, [FromQuery] string FirstName = null, [FromQuery] string LastName = null, [FromQuery] string FavoriteTA = null, [FromQuery] string FavoriteTeacher = null, [FromQuery] string FavoriteClass = null, [FromQuery] bool? IsALiar = null)
+        {
+            if (!IdExists(id))
+                return StatusCode(404, NotFoundMessage);
+            var n = GetNickById(id);
+            var islying = CheckForLies(FavoriteTA, FavoriteTeacher, FavoriteClass);
+            if (IsALiar.HasValue)
+            {
+                if (!IsALiar.Value)
+                {
+                    if (islying.StatusCode == 406)
+                        return islying;
+                }
+            }
+            else if (!n.IAmALiar && islying.StatusCode == 406)
+            {
+                return islying;
+            }
+            if (FirstName != null)
+                n.FirstName = FirstName;
+            if (LastName != null)
+                n.LastName = LastName;
+            if (FavoriteClass != null)
+                n.FavoriteClass = FavoriteClass;
+            if (FavoriteTeacher != null)
+                n.FavoriteTeacher = FavoriteTeacher;
+            if (FavoriteTA != null)
+                n.FavoriteTa = FavoriteTA;
+            if (IsALiar.HasValue)
+                n.IAmALiar = IsALiar.Value;
+            DBContext.Context.NickTable.Update(n);
+            DBContext.Context.SaveChanges();
+            return StatusCode(202, n);
         }
         private int GetNextAvailableID()
         {
@@ -93,17 +114,17 @@ namespace FinalProjectContemporaryProgramming.Controllers
         [HttpDelete]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult Delete([FromQuery]int id)
+        public ActionResult Delete([FromQuery] int id)
         {
-            if(IdExists(id))
+            if (IdExists(id))
             {
                 Get().Remove(GetNickById(id));
                 DBContext.Context.SaveChanges();
-                return Ok(new {Title="Successfully Deleted",Message="Row with ID: "+id+" has been deleted." });
+                return Ok(new { Title = "Successfully Deleted", Message = "Row with ID: " + id + " has been deleted." });
             }
-            return StatusCode(404,NotFoundMessage);
+            return StatusCode(404, NotFoundMessage);
         }
         private bool IdExists(int id) => Get().Any(e => e.Id.Equals(id));
-        private NicksTable GetNickById(int id) =>Get().First(e => e.Id == id);
+        private NicksTable GetNickById(int id) => Get().First(e => e.Id == id);
     }
 }
--- FinalProjectContemporaryProgramming/Program.cs
+++ FinalProjectContemporaryProgramming/Program.cs
@@ -4,18 +4,45 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace FinalProjectContemporaryProgramming
 {
     public class Program
     {
+        [STAThread]
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            if (Prompt())
+                CreateHostBuilder(args).Build().Run();
         }
-
+        private static bool Prompt()
+        {
+            var ofd = new OpenFileDialog()
+            {
+                Filter = "mdf files (*.mdf)|*.mdf|All files (*.*)|*.*"
+            };
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                if (Path.GetExtension(ofd.FileName).ToLower() == ".mdf")
+                {
+                    DBContext.MdfFileLocation = ofd.FileName;
+                    return true;
+                }
+                else
+                {
+                    if (MessageBox.Show("File Must Be An .mdf File", "Invalid File Type", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.ServiceNotification) == DialogResult.Retry)
+                        return Prompt();
+                    return false;
+                }
+            }
+            else
+                return false;
+        }
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
